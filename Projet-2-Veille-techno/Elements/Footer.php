3a7d8b3cb0231824bbed368c2d4f270ff00c62db
Akorus1 <Phiuris1@gmail.com>
2020-07-01 19:20:40 +0200

    Commit n°2 by Kevin

--- Projet-2-Veille-techno/Elements/Footer.php
+++ Projet-2-Veille-techno/Elements/Footer.php
@@ -1,9 +1,9 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 <footer>
 <nav class="navbar fixed-bottom navbar-dark bg-dark">
-<a class="btn btn-dark" href="http://Projet2-solo-solo/Projet-2-Veille-techno/Fonctionnalités_Site/contact.php" role="button">Nous contacter</a>
-<a class="btn btn-dark" href="http://Projet2-solo-solo/Projet-2-Veille-techno/Accueil_et_Mentions_légales/MentionsLegales.php" role="button">Les mentions légales</a>
-<a class="btn btn-dark" href="http://Projet2-solo-solo/Projet-2-Veille-techno/Fonctionnalités_Site/conditions_générales_ventes.php" role="button">Les conditions générales de ventes</a>
+<a class="btn btn-dark" href="http://Projet2-solo/Projet-2-Veille-techno/Fonctionnalités_Site/contact.php" role="button">Nous contacter</a>
+<a class="btn btn-dark" href="http://Projet2-solo/Projet-2-Veille-techno/Accueil_et_Mentions_légales/MentionsLegales.php" role="button">Les mentions légales</a>
+<a class="btn btn-dark" href="http://Projet2-solo/Projet-2-Veille-techno/Fonctionnalités_Site/conditions_générales_ventes.php" role="button">Les conditions générales de ventes</a>
 </nav>
 </footer>
 
